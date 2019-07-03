f051be1b3adb3b853c5aa6243a9acc3f5c8abc18
Obliviann <34093004+Obliviann@users.noreply.github.com>
2019-07-03 11:53:22 +0200

    added user model and service

---
+++ src/app/services/usuario.service.spec.ts
@@ -0,0 +1,12 @@
+  //1.
+  import { TestBed } from '@angular/core/testing';
+  import { UsuarioService } from './usuario.service';
+  
+  describe('UserService', () => {
+      beforeEach(() => TestBed.configureTestingModule({}));
+    
+      it('should be created', () => {
+        const service: UsuarioService = TestBed.get(UsuarioService);
+        expect(service).toBeTruthy();
+      });
+    });
