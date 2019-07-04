943d769f4054142db6ef7ba1e5ffedfc71baead7
luchominervino <luchominervino@hotmail.com>
2019-07-04 17:42:35 +0200

    métodos like y dislike funcionando

--- src/app/components/bienvenida/bienvenida.component.ts
+++ src/app/components/bienvenida/bienvenida.component.ts
@@ -11,31 +11,36 @@ import { UsuarioService } from '../../services/usuario.service';
 })
 export class BienvenidaComponent implements OnInit {
   //2. instanciamos un objeto Usuario
-  user: Usuario;  
+  userSesion: Usuario;
   userList : Array<Usuario>;//esto se llena con this.user = data lin 24
   //3.
   constructor(private router: Router, private usuarioService: UsuarioService) { }
 
   //se ejecuta automáticamente cuando carge la página
   ngOnInit() {
-    this.user = this.usuarioService.getUsuarioLoggedIn();
-    this.usuarioService.getUsersList(this.user.idusuario)
+    this.userSesion = this.usuarioService.getUsuarioLoggedIn();
+    this.usuarioService.getUsersList(this.userSesion.idusuario)
         .subscribe( data => {  //me suscribo a los servicios del método (solo para peticiones http)
          this.userList = data; //data es la inf que me llega, y se la asigno a user
       })
   }
   
-  like(id1: number, id2: number) {             
-    this.usuarioService.like(id1, id2)
+  like(user1: Usuario, user2: Usuario) {   
+
+    console.log(user1);
+
+    this.usuarioService.like(user1.idusuario, user2.idusuario)
         .subscribe( data => { //me suscribo a los servicios del método like
-          this.user = data;   //data es la inf que me llega, y se la asigno a users
+          this.userList = this.userList.filter(u => u !== user2);
+          this.userSesion = data;   //data es la inf que me llega, y se la asigno a users
         })
   }
 
-  dislike(id1: number, id2: number) {
-    this.usuarioService.dislike(id1, id2)
+  dislike(user1: Usuario, user2: Usuario) {
+    this.usuarioService.dislike(user1.idusuario, user2.idusuario)
         .subscribe( data => {
-          this.user = data;
+          this.userList = this.userList.filter(u => u !== user2);
+          this.userSesion = data;
         })
   }
 
