0c3568ad16c785d589c7c0065569a1254c8f798a
luchominervino <luchominervino@hotmail.com>
2019-07-04 10:11:43 +0200

    matches.component.ts

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Usuario } from '../../models/usuario';
-/*import { UserService } from '../../services/uservice';*/
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-matches',
@@ -14,7 +14,7 @@ export class MatchesComponent implements OnInit {
 
   usuarios: Usuario[];
 
-  constructor(private router:Router, private usuarioService: UsuarioService) { 
+  constructor(private router:Router, private userService: UsuarioService) { 
 
   }
 
